0cd482840bbf67d0546f622a3b21fdacf6423afe
Guilherme Salazar <guisalazarr@gmail.com>
2023-05-07 13:31:33 -0300

    ajuste dialog

--- src/components/DaialogConfirm.tsx
+++ src/components/DaialogConfirm.tsx
@@ -6,7 +6,6 @@ import {
   DialogContent,
   DialogContentText,
   DialogTitle,
-  Grid,
   Typography
 } from '@mui/material';
 import React from 'react';
@@ -29,46 +28,37 @@ const DialogConfirm: React.FC<DialogConfirmProps> = ({
   actionConfirm
 }) => {
   return (
-    <Grid container>
-      <Dialog
-        open={openDialog}
-        onClose={actionCancel}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <Box bgcolor="#c4cbb7">
-          <Grid item xs={12}>
-            <DialogTitle id="alert-dialog-title">{title}</DialogTitle>
-          </Grid>
+    <Dialog
+      open={openDialog}
+      onClose={actionCancel}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+    >
+      <Box bgcolor="#c4cbb7" sx={{ textAlign: 'center' }}>
+        <DialogTitle color="black" id="alert-dialog-title">
+          {title}
+        </DialogTitle>
 
-          <DialogContent>
-            <DialogContentText id="alert-dialog-description" sx={{ textAlign: 'center' }}>
-              <>
-                <Grid item xs={12}>
-                  <Typography variant="h4">{subtile}</Typography>
-                </Grid>
-                <Grid item xs={12}>
-                  <Typography variant="body2">{description}</Typography>
-                </Grid>
-              </>
-            </DialogContentText>
-          </DialogContent>
+        <DialogContent>
+          <DialogContentText variant="h4" color="black" id="alert-dialog-description">
+            {subtile}
+          </DialogContentText>
+          <DialogContentText variant="body1" color="black" id="alert-dialog-description">
+            {description}
+          </DialogContentText>
+        </DialogContent>
 
-          <DialogActions>
-            <Grid item xs={6}>
-              <Button variant="contained" onClick={actionCancel} color="secondary" fullWidth>
-                Cancelar
-              </Button>
-            </Grid>
-            <Grid item xs={6}>
-              <Button variant="contained" onClick={actionConfirm} autoFocus fullWidth>
-                Confirmar
-              </Button>
-            </Grid>
-          </DialogActions>
-        </Box>
-      </Dialog>
-    </Grid>
+        <DialogActions>
+          <Button variant="contained" onClick={actionCancel} color="secondary" fullWidth>
+            Cancelar
+          </Button>
+
+          <Button variant="contained" onClick={actionConfirm} fullWidth>
+            Confirmar
+          </Button>
+        </DialogActions>
+      </Box>
+    </Dialog>
   );
 };
 
